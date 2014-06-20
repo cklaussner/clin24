7984013185ca85cc1447a133fcd0f899097c8fca
Cagri Coltekin <c.coltekin@rug.nl>
2014-06-20 17:30:30 +0200

    Added 5-fold R&D experiment files.

---
+++ experiments/5-fold-cv/5-fold-cv.R
@@ -0,0 +1,23 @@
+
+args <- commandArgs(trailingOnly = TRUE)
+basename <- as.character(args[1])
+datadir <- as.character(args[2])
+infile <- paste(datadir, '/', basename, '.txt', sep='')
+outfile <- paste(basename, '.5fold.rda', sep='')
+set1author <- substr(basename, 1, 1)
+
+source("../../code/eval/n-fold-rd.R")
+
+data <- as.matrix(read.table(infile))
+
+rd.name <- paste('rd', gsub('-', '.', basename), sep='.')
+assign(rd.name,
+       n.fold.RD(data, 
+                 substr(rownames(data), 1, 1) == set1author,
+                 substr(rownames(data), 1, 1) != set1author)
+)
+
+data.name <- paste('data', gsub('-', '.', basename), sep='.')
+assign(data.name, data)
+
+save(list=c(data.name, rd.name), file=outfile)
